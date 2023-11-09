ea3a865d866e72d7b5fec0f2fb61e929816ff480
1 <1@DESKTOP-ACMQRLH>
2023-11-09 23:59:10 +0200

    Добавьте файлы проекта.

---
+++ Lab.3.4.mrdium.lvl.kruchinina/Program.cs
@@ -0,0 +1,11 @@
+﻿//24var
+Console.WriteLine("|    x   |   y    |");
+for(double x = -Math.PI / 2; x <= Math.PI; x+=0.2)
+{
+    double y;
+    if (x < 0) y = Math.Exp(-x);
+    else if (x >= 0 && x <= 2) y = -2 * Math.Pow(x, 3) + 2;
+    else y = Math.Sqrt(2*x+Math.Log(x-1));
+    Console.WriteLine($"|{x,8:F2}|{y,8:F2}|");
+    x += 0.2;
+}
